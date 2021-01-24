c426a9215a67cdf5359e3550c80c6fe75c52c644
sfapjliq08 <sfapjliq08@lottechilsung.co.kr>
2021-01-24 17:49:18 +0900

    회원가입, 로그인 수정

--- notice-board/src/routers/user.js
+++ notice-board/src/routers/user.js
@@ -25,7 +25,7 @@ router.post('/login', async (req, res, next) => {
     }
 
     const token = jwt.sign(
-      { email: user.email },
+      { email: user.email, name: user.name },
       'Test123!', // FIXME
       { expiresIn: '1h' }
     );
@@ -37,15 +37,15 @@ router.post('/login', async (req, res, next) => {
   }
 });
 
-router.post('/register', async (req, res) => {
+router.post('/signup', async (req, res) => {
   const { email } = req.body;
-  const user = await UserModel.findOne({ email });
+  const user = await userModel.findOne({ email });
 
   if (user) {
     return res.status(422).json({ message: '이미 등록된 email 입니다.' });
   }
 
-  await new UserModel(req.body).save();
+  await new userModel(req.body).save();
 
   res.status(200).json({ message: '정상적으로 등록되었습니다.' });
 });
